f75ae335a4a2dc922b3a71d557eb7a2702aede1f
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-16 11:03:30 +0600

    output variables

--- 04.PHP Variables/index.php
+++ 04.PHP Variables/index.php
@@ -1,7 +1,11 @@
 <?php
+    /*
     $txt = "Hello World!";
     $x = 4;
-    $y = 5;
+    $y = 5; 
+    */
 
-    
+    // Output Variables
+    $greeting = "Hi, ";
+    echo $greeting." Sujon Ahmed";
 ?>
