691652b1b8378948a5daee7ffb3c8e47c3849f10
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-16 10:04:31 +0600

    case sensitivity

--- 02.PHP Syntax/index.php
+++ 02.PHP Syntax/index.php
@@ -2,4 +2,11 @@
     // PHP code goes here
 
     echo "Hello World!";
+
+    // PHP case sensitivity
+    $color = "red";
+    echo "My car is " . $color . "<br>";
+    echo "My house is " . $COLOR . "<br>";
+    echo "My boat is " . $coLOR . "<br>";
+    
 ?>
