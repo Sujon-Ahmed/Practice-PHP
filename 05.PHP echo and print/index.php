f5cb5ea7edb1f7d58d6192dd688f63c40768ea3d
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-17 10:07:21 +0600

    PHP echo statements

--- 05.PHP echo and print/index.php
+++ 05.PHP echo and print/index.php
@@ -1,5 +1,9 @@
 <?php
+    echo "<h2>PHP is Fun</h2><br>";
+    echo "I'm about to learn PHP!<br>";
+    echo "This ", "string ", "was ", "made ", "with multiple parameters! <br>";
 
-    
-
+    $txt1 = "Sujon Ahmed ";
+    $txt2 = "Riman";
+    echo "My name is ".$txt1.$txt2;
 ?>
