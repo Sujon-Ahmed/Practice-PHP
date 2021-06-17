03e59a841c1c0311069802814a23d6e0f934eb34
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-17 10:41:12 +0600

    PHP String

--- 06.PHP DataType/index.php
+++ 06.PHP DataType/index.php
@@ -1,3 +1,21 @@
 <?php
-    
+    // String
+    // Integer
+    // Float
+    // Boolean
+    // Array // array classes
+    // Object // oop concept
+    // NULL
+    // Resource
+
+    // PHP String
+    echo "HI There! <br>";
+    $x = "Hello ";
+    $y = "World";
+    echo $x.$y."<br>";
+
+  
+  
+
+  
 ?>
