5928f528b28ea36bd94f64ab3333630b4c694fa6
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-17 11:58:29 +0600

    PHP String function

---
+++ 07.PHP Strings/index.php
@@ -0,0 +1,68 @@
+<?php
+    // strlen() - Return the Length of a String
+    echo strlen("Hello_World!");
+    echo "<br>";
+
+    // str_word_count() - Count Words in a String
+    echo str_word_count("This is my first paragraph");
+    echo "<br>";
+
+    // strrev() - Reverse a String
+    echo strrev("reverse");
+    echo "<br>";
+
+    // strpos() - Search For a Text Within a String
+    echo strpos("Hello World", "World");
+    echo "<br>";
+
+    // str_replace() - Replace Text Within a String
+    echo str_replace("World", "Dolly", "Hello World");
+    echo "<br>";
+
+    echo trim(" Hello    World ");
+    echo "<br>";
+
+    $string = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi eaque ea doloribus voluptatem asperiores suscipit dolore. Velit officia excepturi vero, doloremque ex omnis deserunt ad reiciendis quae ratione. Commodi, soluta.";
+
+    // echo $string;
+
+    // $strcut = substr($string,0,30);
+
+    // echo $strcut.'<a href="#">Read More</a>';
+
+    // $valid =  trim($string);
+
+    // if($valid != ''){
+        
+    // }
+
+    // echo strtolower($string);
+    // echo strtoupper($string);
+
+    // $num  = 4525;
+
+    // echo ucfirst($string);
+
+    //echo ucwords($string);
+
+    $name = 'sujon';
+
+    //existing variable check
+    if(isset($name)){
+        echo 'set';
+    }else{
+        echo 'unset';
+    }
+
+    //existing value fill or not
+
+    if(empty($name)){
+        echo 'unset';
+    }else{
+        echo 'set';
+    }
+
+    echo $name;
+
+
+?>
