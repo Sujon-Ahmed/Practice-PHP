d2e2968b327437102cdabec22501cd6fc6696b03
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-17 10:08:41 +0600

    PHP Print Statements

--- 05.PHP echo and print/index.php
+++ 05.PHP echo and print/index.php
@@ -1,4 +1,5 @@
 <?php
+    // PHP echo Statements
     echo "<h2>PHP is Fun</h2><br>";
     echo "I'm about to learn PHP!<br>";
     echo "This ", "string ", "was ", "made ", "with multiple parameters! <br>";
@@ -6,4 +7,7 @@
     $txt1 = "Sujon Ahmed ";
     $txt2 = "Riman";
     echo "My name is ".$txt1.$txt2;
+
+    // PHP print Statements
+
 ?>
