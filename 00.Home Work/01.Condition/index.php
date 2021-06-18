1c93765bd08efb928018fbf01c6f85da6029afdc
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-18 09:06:05 +0600

    Even and Odd number checker

--- 00.Home Work/01.Condition/index.php
+++ 00.Home Work/01.Condition/index.php
@@ -1,6 +1,6 @@
 <?php
 
-    // speed in kmph
+   /*  // Program: 01 - speed in kmph checker program
     $speed = 80;
     if($speed <= 0){
         echo "Your Car is Stop";
@@ -17,6 +17,13 @@
     {
         // when speed is greater than 100
         echo "Its dangerous";
-    }
+    } */
 
-?>
+    // Program : 02 - Even and Odd number checker
+    $num = 3;
+    if($num%2 == 0){
+        echo "$num is Even number<br>";
+    }else{
+        echo "$num is Odd number<br>";
+    }
+?> 	
