938f5a315f2875cb138b95161ee436f66ee4b4f3
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-18 17:33:50 +0600

    PHP Infinity

--- 08.PHP Numbers/index.php
+++ 08.PHP Numbers/index.php
@@ -4,7 +4,11 @@
     // echo var_dump(is_int($num));
 
     // is_float function
-    $num = 4.5;
-    echo var_dump(is_float($num));
+    // $num = 4.5;
+    // echo var_dump(is_float($num));
+
+    // PHP Infinity
+    $x = 1.9e411;
+    echo var_dump($x);
 
 ?>
