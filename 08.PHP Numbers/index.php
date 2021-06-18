049d06d40988362057db7a7d1e4cd0a613ab0d88
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-18 17:31:58 +0600

    is_float function added

--- 08.PHP Numbers/index.php
+++ 08.PHP Numbers/index.php
@@ -1,6 +1,10 @@
 <?php
+    // is_int function
+    // $num = 4564;
+    // echo var_dump(is_int($num));
 
-    $num = 4564;
-    echo var_dump(is_int($num));
+    // is_float function
+    $num = 4.5;
+    echo var_dump(is_float($num));
 
 ?>
