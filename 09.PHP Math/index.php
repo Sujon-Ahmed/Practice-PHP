2296a8afb1a06ab9a39ae338b012ab1f61ce334b
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-18 19:55:30 +0600

    abs function added

--- 09.PHP Math/index.php
+++ 09.PHP Math/index.php
@@ -2,10 +2,14 @@
     // PHP Math
 
     // PI function
-    echo pi();
+    //echo pi();
 
     // PHP min() and max() function
-    
+    // echo min(12,45,-45,445,125,-45,12,50);
+    // echo "<br>";
+    // echo max(45,158,4,155,123,45,45);
+
+    // PHP abs() function
 
 
 ?>
