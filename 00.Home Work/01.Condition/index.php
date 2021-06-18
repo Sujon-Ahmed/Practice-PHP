d67dbdc5b0505fbd7247c5728557bc561397b38f
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-18 08:51:42 +0600

    Check your car speed program

--- 00.Home Work/01.Condition/index.php
+++ 00.Home Work/01.Condition/index.php
@@ -1,6 +1,22 @@
 <?php
 
-    
-
+    // speed in kmph
+    $speed = 80;
+    if($speed <= 0){
+        echo "Your Car is Stop";
+    }
+    elseif($speed < 60)
+    {
+        echo "Safe driving speed";
+    }
+    elseif($speed > 60 && $speed < 100)
+    {
+        echo "You are burning extra fuel";
+    }
+    else 
+    {
+        // when speed is greater than 100
+        echo "Its dangerous";
+    }
 
 ?>
