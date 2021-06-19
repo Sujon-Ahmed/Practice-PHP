7c81004364b6d3276dc478bf1a9c114b0a8f0463
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-19 07:31:18 +0600

    PHP Constants

---
+++ 10.PHP Constants/index.php
@@ -0,0 +1,11 @@
+<?php
+    // PHP Constants case-sensitive
+    //define("GREETING","Welcome to this page!");
+    //echo GREETING;
+
+    // PHP Constants case-insensitive
+    //define("Greeting", "Hello There!",true);
+    //echo greeting;
+
+
+?>
