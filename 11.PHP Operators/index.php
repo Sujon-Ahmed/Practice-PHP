f7576c2c8769c33d6d23a02cf4ed0b2c890bbc26
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-20 09:14:04 +0600

    increment and decrement example

--- 11.PHP Operators/index.php
+++ 11.PHP Operators/index.php
@@ -29,14 +29,25 @@
     // $y = 10;
     // echo var_dump($x <=> $y);
 
-    $x = 10;
+    /* $x = 10;
     $y = 10;
     //type check    
     if($x === $y){
         echo 'match';
     }else{
         echo "don't match";
-    }
+    } */
+
+    // Increment and Decrement Operators
+    // pre-increment
+    // $x = 10;
+    // echo ++$x;
+
+    // post-increment
+    // $y = 10;
+    // echo $y++;
+
+    // same-as decrement
 
 
 ?>
