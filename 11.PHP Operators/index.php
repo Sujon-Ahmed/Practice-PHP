9504bcc39ab77153b40734149890f8133357868c
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-20 08:43:31 +0600

    Example Assignment Operators

--- 11.PHP Operators/index.php
+++ 11.PHP Operators/index.php
@@ -1,18 +1,24 @@
 <?php
     // Arithmetic Operators
-    $x = 30;
-    $y = 20;
+    // $x = 30;
+    // $y = 20;
 
-    echo $x + $y;
-    echo "<br>";
-    echo $x - $y;
-    echo "<br>";
-    echo $x * $y;
-    echo "<br>";
-    echo $x / $y;
-    echo "<br>";
-    echo $x % $y;
-    echo "<br>";
+    // echo $x + $y;
+    // echo "<br>";
+    // echo $x - $y;
+    // echo "<br>";
+    // echo $x * $y;
+    // echo "<br>";
+    // echo $x / $y;
+    // echo "<br>";
+    // echo $x % $y;
+    // echo "<br>";
+
+    // Assignment Operators
+    $x = 10;
+    $y = 20;
+    $x +=$y;
+    echo $x;
 
 
 ?>
