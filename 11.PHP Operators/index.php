ce30cb944017dfc982b8b5fb0aea1b9223aae733
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-20 08:53:28 +0600

    with spaceShip

--- 11.PHP Operators/index.php
+++ 11.PHP Operators/index.php
@@ -21,9 +21,13 @@
     // echo $x;
 
     // Comparison Operators
-    $x = 10;
+    // $x = 10;
+    // $y = 10;
+    // echo var_dump($x == $y);
+
+    $x = 15;
     $y = 10;
-    echo var_dump($x == $y);
+    echo var_dump($x <=> $y);
 
 
 ?>
