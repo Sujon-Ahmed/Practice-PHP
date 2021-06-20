b0fe4d13700773ac88f5fe7c96fb2afa21174129
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-20 08:38:58 +0600

    Example PHP Arithmetic Operators

---
+++ 11.PHP Operators/index.php
@@ -0,0 +1,18 @@
+<?php
+    // Arithmetic Operators
+    $x = 30;
+    $y = 20;
+
+    echo $x + $y;
+    echo "<br>";
+    echo $x - $y;
+    echo "<br>";
+    echo $x * $y;
+    echo "<br>";
+    echo $x / $y;
+    echo "<br>";
+    echo $x % $y;
+    echo "<br>";
+
+
+?>
