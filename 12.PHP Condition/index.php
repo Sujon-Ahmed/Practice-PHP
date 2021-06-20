3a6f3bd8eb132c11291eb2928c97020fc2d06ada
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-20 10:55:53 +0600

    done all condition

---
+++ 12.PHP Condition/index.php
@@ -0,0 +1,28 @@
+<?php
+    // if condition
+    // $t = date("H");
+    // if($t < "20"){
+    //     echo "Have a good day!";
+    // }
+
+     //if...else condition
+    //  $t = date("H");
+    // if($t < "20"){
+    //     echo "Have a good day!";
+    // }else{
+    //     echo "Have a good night!";
+    // }
+
+    //if...elseif else condition
+    // $t = date("H");
+    // if($t < "10"){
+    //     echo "Have a good morning!";
+    // }
+    // elseif($t < "20"){
+    //     echo "Have a good day!";
+    // }else{
+    //     echo "Have a good night!";
+    // }
+
+
+?>
