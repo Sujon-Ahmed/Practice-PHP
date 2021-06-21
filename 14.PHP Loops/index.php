13b3fff4385b5824aeff23df1063b54693282940
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-21 18:47:37 +0600

    while loop game done

--- 14.PHP Loops/index.php
+++ 14.PHP Loops/index.php
@@ -1,9 +1,23 @@
 <?php
     // while loop example
-    $x = 1;
-    while($x <= 10){
-        echo "The number is $x <br>";
-        $x++;
-    }
+    // $x = 1;
+    // while($x <= 10){
+    //     echo "The number is $x <br>";
+    //     $x++;
+    // }
+
+    // while loop game
+   $value = true;
+   $number = 6;
+   while($value){
+       $randomNumber = rand(1,10);
+       if ($randomNumber == $number) {
+          echo "Winner You Have Checkin Dinner";
+          break;
+       }
+       else{
+           echo "You have got : $randomNumber <br>";
+       }
+   }
 
 ?>
