d738ae7f7152fdc8975828b04a1935fa5bf0f4b2
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-21 18:51:08 +0600

    numbering system step 10 to 100

--- 14.PHP Loops/index.php
+++ 14.PHP Loops/index.php
@@ -7,17 +7,24 @@
     // }
 
     // while loop game
-   $value = true;
-   $number = 6;
-   while($value){
-       $randomNumber = rand(1,10);
-       if ($randomNumber == $number) {
-          echo "Winner You Have Checkin Dinner";
-          break;
-       }
-       else{
-           echo "You have got : $randomNumber <br>";
-       }
-   }
+//    $value = true;
+//    $number = 6;
+//    while($value){
+//        $randomNumber = rand(1,10);
+//        if ($randomNumber == $number) {
+//           echo "Winner You Have Checkin Dinner";
+//           break;
+//        }
+//        else{
+//            echo "You have got : $randomNumber <br>";
+//        }
+//    }
+
+    // print 0 to 100 number and always print step 10 
+    $number = 0;
+    while($number <= 100){
+        echo "The number is $number <br>";
+        $number+=10;
+    }
 
 ?>
