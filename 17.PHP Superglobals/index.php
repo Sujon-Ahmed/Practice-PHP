52b7bd47e79b5a449e8cfedfeca6c0b9aff3919d
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-22 21:12:10 +0600

    super global

--- 17.PHP Superglobals/index.php
+++ 17.PHP Superglobals/index.php
@@ -1,13 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="fname">
+  <input type="submit">
+</form>
+
 <?php
-
-    $x = 75;
-    $y = 45;
-
-    function addition(){
-        $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // collect value of input field
+        $name = htmlspecialchars($_REQUEST['fname']);
+        if (empty($name)) {
+            echo "Name is empty";
+        } else {
+            echo $name;
+        }
     }
-   addition();
-   echo $z;
 
+    //     $x = 75;
+    //     $y = 45;
 
+    //     function addition(){
+    //         $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+    //     }
+    //    addition();
+    //    echo $z;
+
+    // echo $_SERVER['PHP_SELF'];
 ?>
+
+</body>
+</html>
