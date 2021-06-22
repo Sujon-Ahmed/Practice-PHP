8fd8824b438d82bbce6ed2b8548c1115fc5f3c1b
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-22 07:44:00 +0600

    insert array

--- 16.PHP Array/index.php
+++ 16.PHP Array/index.php
@@ -1,14 +1,17 @@
 <?php
 
     //array declare
-    1. $array = array();
-    2. $array = [];
+    //1. $array = array();
+    //2. $array = [];
 
     // create and call array
     //$array = array('hi','hello','hola');
     // echo "<pre>";
     // print_r($array);
     // echo "</pre>";
+
+    // array insert
+    echo $arr[1] = 45;
 
     // count() function
     //echo count($array);
