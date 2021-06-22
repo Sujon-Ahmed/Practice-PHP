02a8f7ea8aefc8723bb23161bc49e2fa7f77156d
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-22 09:39:39 +0600

    array key

--- 16.PHP Array/index.php
+++ 16.PHP Array/index.php
@@ -29,16 +29,37 @@
     //echo "Sujon is " . $age['Sujon'] . " years old.";
     
     // Loop Thorough an Associative Array
-    $age = array("Sujon"=>"21", "Toukir"=>"22", "Alamin"=>"23");
-    foreach($age as $key => $val){
-        echo "Key=" . $key . ", Value=" . $val;
-        echo '<br>';
-    }
+    // $age = array("Sujon"=>"21", "Toukir"=>"22", "Alamin"=>"23");
+    // foreach($age as $key => $val){
+    //     echo "Key=" . $key . ", Value=" . $val;
+    //     echo '<br>';
+    // }
 
 
 
     // count() function
     //echo count($array);
+
+    // $arr = [
+    // ['rahim','karim','rasel'],
+    // [1,2,3,4,5,6],
+    // ['volvo']
+    // ];
+
+    //$arr[];
+    // echo '<pre>';
+    //     print_r($arr[][][]);
+    // echo '<pre>';
+
+    // array_key() use for see all key in a array
+    // array_key_first() use for see first key in a array
+    // array_key_last() use for see last key in a array
+    //array_key_exists('key',$array) use for search key in a array
+    //key_exists() use for search key in a array
+
+    // $array = ['red','green','blue','brown'];
+
+    // print_r(array_keys($array));
 
 
 
