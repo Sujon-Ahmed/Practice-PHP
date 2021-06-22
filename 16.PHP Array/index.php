7131947b17d5d84285ebc70e888a1f03181d9f2f
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-22 07:39:33 +0600

    count function add

---
+++ 16.PHP Array/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    // create and call array
+    $array = array('hi','hello','hola');
+    // echo "<pre>";
+    // print_r($array);
+    // echo "</pre>";
+    
+    // count() function
+    echo count($array);
+
+
+?>
