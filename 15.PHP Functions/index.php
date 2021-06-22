50abb07426e75aefe79c787946225b3ddafb0d66
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-22 07:29:19 +0600

    function argument

--- 15.PHP Functions/index.php
+++ 15.PHP Functions/index.php
@@ -6,13 +6,29 @@
     // writeMsg(); // call the function
 
     //Function Arguments
-    function friendsName($firstName){
-        echo "$firstName Ahmed <br>";
-    }
-    friendsName('Sujon');
-    friendsName('Toukir');
-    friendsName('Alamin');
+    // function friendsName($firstName){
+    //     echo "$firstName Ahmed <br>";
+    // }
+    // friendsName('Sujon');
+    // friendsName('Toukir');
+    // friendsName('Alamin');
 
+    // more example
+    // function familyName($fname, $year) {
+    //     echo "$fname Miah. Born in $year <br>";
+    //   }
+      
+    //   familyName("Nur","1924");
+    //   familyName("Lal","1975");
+    //   familyName("Sujon","2000");
+
+    // strict requirement
+    declare(strict_types=1);
+    function addNumbers(int $a, int $b){
+        $c = $a + $b;
+        return $c;
+    }
+    echo addNumbers(10,10);
 
 
 ?>
