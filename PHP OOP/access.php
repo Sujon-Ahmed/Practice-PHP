82205fe7f8c757dcbd247486a1a3ad85a76f042a
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-07-06 15:40:23 +0600

    access modifiers example

--- PHP OOP/access.php
+++ PHP OOP/access.php
@@ -1,5 +1,23 @@
 <?php
-    
+    class Fruit {
+        public $name;
+        protected $color;
+        private $weight;
+
+        public function set_name($name) {
+            $this->name = $name;
+        }
+        protected function set_color($color) {
+            $this->color = $color;
+        }
+        private function set_weight($weight) {
+            $this->weight = $weight;
+        }
+    }
+    $obj = new Fruit();
+    $obj->set_name = 'Mango';
+    $obj->set_color = 'yellow';
+    $obj->set_weight = '300';
 
 
 
