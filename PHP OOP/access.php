4a4d2f098c689a11551c0eabf976bc017b1ab2ee
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-07-07 10:38:45 +0600

    access modifiers

--- PHP OOP/access.php
+++ PHP OOP/access.php
@@ -1,5 +1,5 @@
 <?php
-    class Fruit {
+    /* class Fruit {
         public $name;
         protected $color;
         private $weight;
@@ -17,7 +17,32 @@
     $obj = new Fruit();
     $obj->set_name = 'Mango';
     $obj->set_color = 'yellow';
-    $obj->set_weight = '300';
+    $obj->set_weight = '300'; */
+
+    class fruit{
+        public $name;
+        protected $color;
+        public function __construct($name,$color){
+            $this->name = $name;
+            $this->color = $color;
+        }
+        public function intro(){
+            echo "I am {$this->name} and my color is {$this->color}<br>";
+        }
+    }
+    $obj = new fruit('Mango','Yellow');
+    $obj->intro();
+
+
+    // class mango extends fruit{
+    //     public function message(){
+    //         echo 'Who i am? <br>';
+    //     }
+    // }
+
+    // $obj = new mango('mango','yellow');
+    // $obj->message();
+    // $obj->intro();
 
 
 
